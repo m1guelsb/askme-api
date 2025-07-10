4a5818e33bf1984469f96aefbcd1fa9a6cad9984
Miguel Barbosa <miguelbarbosacs@gmail.com>
2025-07-10 10:24:48 -0300

    feat: add create room route

---
+++ src/http/routes/create-room.ts
@@ -0,0 +1,39 @@
+import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
+import { z } from 'zod/v4';
+import { db } from '../../db/connection.ts';
+import { schema } from '../../db/schema/index.ts';
+
+export const createRoomRoute: FastifyPluginCallbackZod = (app) => {
+  app.post(
+    '/rooms',
+    {
+      schema: {
+        body: z.object({
+          name: z.string().min(1, 'Room name is required'),
+          description: z.string().optional(),
+        }),
+      },
+    },
+    async ({ body }, reply) => {
+      const { name, description } = body;
+
+      const result = await db
+        .insert(schema.rooms)
+        .values({
+          name,
+          description,
+        })
+        .returning();
+
+      const newRoom = result[0];
+
+      if (!newRoom) {
+        throw new Error('Failed to create room');
+      }
+
+      return reply.status(201).send({
+        roomId: newRoom.id,
+      });
+    }
+  );
+};
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 import { env } from './env.ts';
+import { createRoomRoute } from './http/routes/create-room.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -21,6 +22,7 @@ app.get('/health', () => {
   return 'OK';
 });
 
-app.register(getRoomsRoute)
+app.register(getRoomsRoute);
+app.register(createRoomRoute);
 
-app.listen({ port: env.PORT || 3001 })
+app.listen({ port: env.PORT || 3001 });
